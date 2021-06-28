5d4e629de3d867e00f0471ca83806af997770310
Igor Ilgiyaev <bigshowmusic@gmail.com>
2021-06-28 18:59:56 +0300

    1

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>To Do List About</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis ipsam reiciendis architecto. Ipsum voluptatem illum dolor esse tempora sed at quam officia veniam aut corporis culpa soluta, incidunt accusantium aliquid?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis ipsam reiciendis architecto. Ipsum voluptatem illum dolor esse tempora sed at quam officia veniam aut corporis culpa soluta, incidunt accusantium aliquid?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis ipsam reiciendis architecto. Ipsum voluptatem illum dolor esse tempora sed at quam officia veniam aut corporis culpa soluta, incidunt accusantium aliquid?</p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="">
+    <v-list
+      class="pt-0"
+      flat
+    >
+      <div
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        <v-list-item
+          @click="taskDone(task.id)"
+          :class="{ 'blue lighten-5' : task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content
+              :class="{ 'text-decoration-line-through' : task.done }"
+            >
+              <v-list-item-title>{{task.title}}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>    
+  </div>  
+</template>
+
+<script>
+  export default {
+    name: 'Todo',
+    data: () => ({ 
+      tasks: [
+        {
+          id: 1,
+          title: 'Wake Up',
+          done: false
+        },
+        {
+          id: 2,
+          title: 'Brush Teeth',
+          done: true
+        },
+        {
+          id: 3,
+          title: 'Go To Work',
+          done: false
+        },
+      ]
+    }),
+    methods:{
+      taskDone(id){
+        let task = this.tasks.filter(task => task.id === id)[0]
+        task.done = !task.done 
+      }
+    }
+  }
+</script>
