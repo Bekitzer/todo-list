f858e02ad20d3b03e3a90b3f9a88f372c9c2f4d2
Igor <office@ng-ws.com>
2021-10-27 00:08:45 +0300

    alpha 0.3.9

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,17 +1,73 @@
 <template>
-  <div class="pa-10">
-    <p>{{ order.number }}</p>
-    <p>{{ order.clientName }}</p>
-    <p>{{ order.orderWork }}</p>
-    <p>{{ order.supplierName }}</p>
-    <p>{{ order.deliveryDate }}</p>
-    <p>{{ order.deliveryType }}</p>
-    <p>{{ order.deliveryAgent }}</p>
-    <p>{{ order.sellPrice }}</p>
-    <p>{{ order.buyPrice }}</p>
-    <p>{{ order.margin }}</p>
-    <p>{{ order.orderCreationDate }}</p>
-    <p>{{ order.orderUpdated }}</p>
+  <div class="pr-10 pl-10">
+    <nav-appbar :pname="pageName"/>
+    <v-row>
+      <v-col cols="12" md="7" sm="7">
+        <v-row class="pa-10 grey lighten-3 rounded-b-xl">
+          <v-col cols="3" md="3" sm="3">
+            <h2>לקוח</h2>
+            <h3>{{ order.clientName }}</h3>
+          </v-col>
+          <v-col cols="3" md="3" sm="3">
+            <h2>ספק</h2>
+            <h3>{{ order.supplierName }}</h3>
+          </v-col>
+          <v-col cols="2" md="2" sm="2">
+            <p>
+              מ.הזמנה > {{ order.number }}
+            </p>
+            <p>
+              מכירה > {{ order.sellPrice }}
+            </p>
+            <p>
+              קנייה > {{ order.buyPrice }}
+            </p>
+            <p>
+              רווח > {{ order.margin }}
+            </p>
+          </v-col>
+          <v-col cols="4" md="4" sm="4">
+            <p>
+              סוג משלוח > {{ order.deliveryType }}
+            </p>
+            <p>
+              אחראי > {{ order.deliveryAgent }}
+            </p>
+              <v-row>
+                <v-col cols="6" md="6" sm="6">
+                  <p style="font-size:14px;">ת.רישום <br />{{ order.orderCreationDate }}</p>
+                </v-col>
+                <v-col cols="6" md="6" sm="6">
+                  <p style="font-size:14px;">ת.עידכון <br />{{ order.orderUpdated }}</p>
+                </v-col>
+              </v-row>
+          </v-col>
+        </v-row>
+        <v-row class="pt-6">
+          <v-expansion-panels flat style="border:1px solid 0 1px 1px 1px">
+            <v-expansion-panel>
+              <v-expansion-panel-header>
+                מוצר
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row>
+                  <v-col cols="6" md="6" sm="6">
+                    <p style="font-size:14px;">אודות ההזמנה <br />{{ order.orderWork }}</p>
+                  </v-col>
+                  <v-col cols="6" md="6" sm="6">
+                    <p style="font-size:14px;">תאריך אספקה <br />{{ order.deliveryDate }}</p>
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="5" sm="5">
+      </v-col>
+    </v-row>
     <v-speed-dial
       v-model="fab"
       bottom
@@ -77,7 +133,7 @@
 
 <script>
 export default {
-  name: 'Client',
+  name: 'Order',
   data: () => ({
     pageName: '',
     fab: false,
@@ -94,10 +150,20 @@ export default {
   },
   components: {
       'dialog-edit': require('@/components/Orders/Dialogs/DialogEdit.vue').default,
-      'dialog-delete': require('@/components/Orders/Dialogs/DialogDelete.vue').default
+      'dialog-delete': require('@/components/Orders/Dialogs/DialogDelete.vue').default,
+      'nav-appbar' : require('@/components/Global/AppBar.vue').default
   },
   mounted() {
-    this.pageName = this.order.number
+    this.pageName = 'מספר הזמנה > ' + this.order.number
   }
 }
 </script>
+<style lang="sass" scoped>
+  .v-expansion-panel
+    border-radius: 0px 0px 20px 20px !important
+  .theme--light.v-expansion-panels .v-expansion-panel
+    border-width: 0px 1px 1px 1px !important
+    border-style: solid !important
+    border-color: #dbdbdb !important
+
+</style>
