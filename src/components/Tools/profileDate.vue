caa84ee692b4da15caa5b20179b5766e3ad9950d
Igor <office@ng-ws.com>
2021-10-31 00:24:43 +0300

    alpha 0.4.9 FIxed timeOfDay to show day periods

--- src/components/Tools/profileDate.vue
+++ src/components/Tools/profileDate.vue
@@ -12,14 +12,35 @@ export default {
             timeOfDay: ''
         }
     },
+    // computed: {
+    //   timeOfDay(){
+    //     if(this.timeOfDay < 12 && this.timeOfDay > 6){
+    //       return 'בוקר טוב'
+    //     }else if(this.timeOfDay > 12 && this.timeOfDay < 18){
+    //       return 'צהריים טובים'
+    //     }else if(this.timeOfDay > 18 && this.timeOfDay < 22){
+    //       return 'ערב טוב'
+    //     }else if(this.this.timeOfDay > 22 && this.timeOfDay < 6){
+    //       return 'לילה טוב'
+    //     }
+    //   }
+    // },
     methods: {
-        getDate() {
-            this.timeOfDay = format(new Date(), 'a', {locale: he})
+      getDate() {
+        this.timeOfDay = format(new Date(), 'HH', {locale: he})
+        if(this.timeOfDay >= 0 && this.timeOfDay < 6){
+          this.timeOfDay = 'לילה טוב'
+        }else if(this.timeOfDay >= 6 && this.timeOfDay < 12){
+          this.timeOfDay = 'בוקר טוב'
+        }else if(this.timeOfDay >= 12 && this.timeOfDay < 18){
+          this.timeOfDay = 'צהריים טובים'
+        }else if(this.timeOfDay >= 18 && this.timeOfDay < 0){
+          this.timeOfDay = 'ערב טוב'
         }
+      }
     },
     mounted() {
-        this.getDate()
-
+      this.getDate()
     }
 }
 </script>
