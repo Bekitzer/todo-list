3aea64e5b44cc780be3e4351531a889df3378b29
moshequ <moshequ@gmail.com>
2022-03-12 08:11:59 +0200

    Reformat

--- src/components/Suppliers/Dialogs/DialogEdit.vue
+++ src/components/Suppliers/Dialogs/DialogEdit.vue
@@ -120,8 +120,7 @@
               <template v-slot:selection="data">
                 <v-chip close @click:close="remove(data.item)">
                   <v-avatar left>
-                    <v-img :src="data.item.avatar"
-                           lazy-src="/images/gravatar.jpg"></v-img>
+                    <v-img :src="data.item.avatar" lazy-src="/images/gravatar.jpg"></v-img>
                   </v-avatar>
                   {{ data.item.username }}
                 </v-chip>
@@ -132,9 +131,7 @@
                 </template>
                 <template v-else>
                   <v-list-item-avatar>
-                    <v-img
-                        :src="data.item.avatar"
-                        lazy-src="/images/gravatar.jpg"></v-img>
+                    <v-img :src="data.item.avatar" lazy-src="/images/gravatar.jpg"></v-img>
                   </v-list-item-avatar>
                   <v-list-item-content>
                     <v-list-item-title v-html="data.item.firstname + ' ' + data.item.lastname"></v-list-item-title>
