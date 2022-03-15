eaa19412ebe5dccee0cfb84661f8a776ebb101a3
moshequ <moshequ@gmail.com>
2022-03-15 16:07:27 +0200

    {{ rate.min_units }}

--- src/config.js
+++ src/config.js
@@ -5,9 +5,44 @@ const defaults = {
 }
 
 const config = {
-  development: {},
+  development: {
+    firebaseConfig: {
+      productionTip: true,
+      apiKey: "AIzaSyA3PX7Z-yWGJSvbecML9EwaeaFCirb-_Kk",
+      authDomain: "just-print-ngs-dev.firebaseapp.com",
+      projectId: "just-print-ngs-dev",
+      storageBucket: "just-print-ngs-dev.appspot.com",
+      messagingSenderId: "664580712157",
+      appId: "1:664580712157:web:cf312b122523907c49dd34",
+      measurementId: "G-NQTE2DSY6E"
+    }
+  },
+  testing: {
+    firebaseConfig: {
+      productionTip: false,
+      apiKey: "AIzaSyC7Czu5PHlhwmfgxCQWggC-gIxEPNEQBA4",
+      authDomain: "just-print-ngs-test.firebaseapp.com",
+      projectId: "just-print-ngs-test",
+      storageBucket: "just-print-ngs-test.appspot.com",
+      messagingSenderId: "668365241999",
+      appId: "1:668365241999:web:cd3933d00274ab97813afa",
+      measurementId: "G-RR4GJ1X1JJ"
+    }
+  },
   staging: {},
-  production: {}
+  production: {
+    firebaseConfig: {
+      productionTip: false,
+      apiKey: "AIzaSyDKRDIueceXgFm4DCCo6Tt1_fTiG_RQ_Uc",
+      authDomain: "just-print-ngs.firebaseapp.com",
+      databaseURL: "https://just-print-ngs-default-rtdb.firebaseio.com",
+      projectId: "just-print-ngs",
+      storageBucket: "just-print-ngs.appspot.com",
+      messagingSenderId: "38410968724",
+      appId: "1:38410968724:web:a56800ce30fbd46dc55e81",
+      measurementId: "G-LS8EQX32HV"
+    }
+  }
 }
 
 export default {...defaults, ...config[env]}
--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,9 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics";
+import config from '@/config'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDKRDIueceXgFm4DCCo6Tt1_fTiG_RQ_Uc",
-  authDomain: "just-print-ngs.firebaseapp.com",
-  databaseURL: "https://just-print-ngs-default-rtdb.firebaseio.com",
-  projectId: "just-print-ngs",
-  storageBucket: "just-print-ngs.appspot.com",
-  messagingSenderId: "38410968724",
-  appId: "1:38410968724:web:a56800ce30fbd46dc55e81",
-  measurementId: "G-LS8EQX32HV"
-}
-
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(config.firebaseConfig);
 const db = getFirestore(firebaseApp);
 const storage = getStorage(firebaseApp);
 const analytics = getAnalytics(firebaseApp);
