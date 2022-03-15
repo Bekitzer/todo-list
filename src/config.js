9c07519f306d2fa8d3d35fc15daae9993e40ef03
moshequ <moshequ@gmail.com>
2022-03-15 09:32:19 +0200

    log env

--- src/config.js
+++ src/config.js
@@ -1,12 +1,14 @@
 const env = process.env.NODE_ENV || 'development'
 
+console.log(process.env.NODE_ENV)
+
 const defaults = {
   DATE_FORMAT: 'EEEEE, dd/MM/yy'
 }
 
 const config = {
+  ...defaults,
   development: {
-    ...defaults
   }
 }
 
